61c3f4899a2803ca8d658784d0b242e1a136c8db
abp46pitt <abp46@pitt.edu>
2024-01-14 16:12:35 -0500

    added bootstrap and header component

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [ 
+    BrowserModule,
+    NgbModule
+  ],
+  declarations: [  ],
+  bootstrap:    [  ]
+})
+export class AppModule { }
