d05c5f5a2994422a8d933b35bdda5f2591b389e1
abp46pitt <abp46@pitt.edu>
2024-01-14 16:35:35 -0500

    footer component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NewHeaderComponent } from './new-header/new-header.component';
+import { NewFooterComponent } from './new-footer/new-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewHeaderComponent],
+  imports: [CommonModule, RouterOutlet, NewHeaderComponent, NewFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
